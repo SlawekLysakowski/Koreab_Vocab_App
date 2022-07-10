b0271a229ca1ee6ec1375ebc139c16e031471f71
SlawekLysakowski <slawku1@gmail.com>
2022-07-10 21:58:16 +0200

    Initial commit

---
+++ src/main/java/pl/edu/wszib/koreab_vocab_app/KoreabVocabAppApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.koreab_vocab_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoreabVocabAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KoreabVocabAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/edu/wszib/koreab_vocab_app/KoreabVocabAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.koreab_vocab_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoreabVocabAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
